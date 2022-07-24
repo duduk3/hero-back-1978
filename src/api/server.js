0de5e808300bc9c76a184b637c51738d23874a76
EDUARDO FERREIRA <dupearljam@gmail.com>
2022-07-24 10:29:25 -0300

    altera log

--- src/api/server.js
+++ src/api/server.js
@@ -5,4 +5,4 @@ require('dotenv/config');
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`));
+app.listen(PORT, () => console.log(`Rodando em ${PORT}`));
