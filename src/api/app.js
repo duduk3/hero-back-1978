3d893e39c5bb73559fe667393cd5c92edff344fb
EDUARDO FERREIRA <dupearljam@gmail.com>
2022-07-25 11:14:47 -0300

    add routes: delete e getById

--- src/api/app.js
+++ src/api/app.js
@@ -9,7 +9,9 @@ const app = express();
 app.use(cors());
 
 app.get('/users', rescue(UserController.getAll));
+app.get('/:id', rescue(UserController.getById));
 app.post('/users', rescue(UserController.create));
+app.post('/:id', rescue(UserController.remove));
 
 app.use(errorMiddleware); 
 
--- src/controllers/user.js
+++ src/controllers/user.js
@@ -5,12 +5,27 @@ const getAll = async (_req, res) => {
     res.status(200).json(users);
 };
 
+const getById = async (req, res) => {
+    const { id } = req.params;
+    const result = await User.getByPk(id);
+    res.status(200).json(result);
+};
+
 const create = async (req, res) => {
-    const users = await User.create(req.body);
+    const { firstName, lastName, email } = req.body;
+    const users = await User.create(firstName, lastName, email);
     res.status(201).json(users);
+};
+
+const remove = async (req, res) => {
+    const { id } = req.params;
+    const result = await User.delete(id);
+    res.status(204).json(result);
 };
 
 module.exports = {
     getAll,
+    getById,
     create,
+    remove,
 };
